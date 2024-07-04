85729c04529da33bab61ea222b661575ab3580e8
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-07-04 20:57:19 +0300

    dom manipulation

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.body.remove("main")
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "ABDULMAJID AMAN HUSSEIN is the champion"
